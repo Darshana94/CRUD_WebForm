daea9e8b0988a2d460a640e2d8a4fd167075a7c6
Darshana94 <darshanapk94@yahoo.com>
2019-07-25 10:41:43 +0530

    first

---
+++ CRUD_WebForm/contact.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="contact.aspx.cs" Inherits="CRUD_WebForm.contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <h1>Users Form</h1><br/>
+    <form id="form1" runat="server">
+        <asp:Label ID="msgSuccess" runat="server" Text="" ForeColor="Green"></asp:Label><br/>
+        <asp:Label ID="msgFail" runat="server" Text="" ForeColor="Red"></asp:Label><br/>    
+    <div class="row">
+        <div class="col-md-6">
+
+            <asp:HiddenField ID="contactId" runat="server" />
+        <table class="table">
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtName" runat="server" CssClass="form-control"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="Address"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtAddress" runat="server" CssClass="form-control" TextMode="MultiLine"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label3" runat="server"  Text="Date of Birth"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtdob" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                    &nbsp;</td>
+                <td colspan="2">
+                    <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn btn-success" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" CssClass="btn btn-danger" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" CssClass="btn btn-light" OnClick="btnClear_Click" />
+                </td>
+            </tr>
+            <%--<tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="msgSuccess" runat="server" Text="" ForeColor="Green"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="msgFail" runat="server" Text="" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>--%>
+        </table>
+
+        </div>
+        <div class="col-md-6">
+            <asp:GridView ID="gv" runat="server" AutoGenerateColumns="false" CssClass="table table-responsive table-hover">
+            <Columns>
+                <asp:BoundField DataField="name" HeaderText="Name" />
+                <asp:BoundField DataField="address" HeaderText="Address" />
+                <asp:BoundField DataField="dob" HeaderText="Date of Birth" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="linkView" runat="server" CommandArgument='<%# Eval("uid") %>' OnClick="linkview">View</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            </asp:GridView>
+        </div>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ CRUD_WebForm/contact.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_WebForm
+{
+    public partial class contact : System.Web.UI.Page
+    {
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=db_test;Integrated Security=True"); 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillGrid();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        public void clear()
+        {
+            contactId.Value = "";
+            txtName.Text = txtAddress.Text = txtdob.Text = "";
+            msgSuccess.Text = msgFail.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+            try {
+                if (sqlcon.State == ConnectionState.Closed)
+                    sqlcon.Open();
+                SqlCommand sqlcmd = new SqlCommand("saveandupdate", sqlcon);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                sqlcmd.Parameters.AddWithValue("@id", (contactId.Value == "" ? 0 : Convert.ToInt32(contactId.Value)));
+                sqlcmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                sqlcmd.Parameters.AddWithValue("@add", txtAddress.Text.Trim());
+                sqlcmd.Parameters.AddWithValue("@dob", txtdob.Text);
+                sqlcmd.ExecuteNonQuery();
+                sqlcon.Close();
+
+                string cid = contactId.Value;
+                clear();
+                if (cid == "")
+                    msgSuccess.Text = "Saved Successfully!!!";
+                else
+                    msgSuccess.Text = "Updated Successfully!!!";
+
+                FillGrid();
+            }
+            catch (Exception ex){
+                msgFail.Text = ex.Message;
+                
+            }
+            
+        }
+
+        void FillGrid() {
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlDataAdapter sqlAd = new SqlDataAdapter("viewall", sqlcon);
+            sqlAd.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            sqlAd.Fill(dt);
+            sqlcon.Close();
+
+            gv.DataSource = dt;
+            gv.DataBind();
+        }
+
+        protected void linkview(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32((sender as LinkButton).CommandArgument);
+
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlDataAdapter sqlAd = new SqlDataAdapter("viewbyid", sqlcon);
+            sqlAd.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlAd.SelectCommand.Parameters.AddWithValue("@id", id); 
+            DataTable dt = new DataTable();
+            sqlAd.Fill(dt);
+            sqlcon.Close();
+
+            contactId.Value = id.ToString();
+            txtName.Text = dt.Rows[0]["name"].ToString();
+            txtAddress.Text = dt.Rows[0]["address"].ToString();
+            txtdob.Text = dt.Rows[0]["dob"].ToString();
+
+            msgSuccess.Text = msgFail.Text = "";
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlCommand sqlcmd = new SqlCommand("deletebyid", sqlcon);
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+
+            sqlcmd.Parameters.AddWithValue("@id", (contactId.Value == "" ? 0 : Convert.ToInt32(contactId.Value)));
+            sqlcmd.ExecuteNonQuery();
+            sqlcon.Close();
+
+            clear();
+            FillGrid();
+            msgSuccess.Text = "Deleted Successfully!!!";
+
+        }
+    }
+}
